cf396b4048ecf98b1bbd0475b64a9b9877e54b08
kviuff <kanglan@leimingtech.com>
2018-08-30 14:20:25 +0800

    添加redis

---
+++ base-api/src/main/java/com/kviuff/api/config/CorsConfig.java
@@ -0,0 +1,40 @@
+package com.kviuff.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * 配置允许ajax跨域请求
+ *
+ * @author kanglan
+ * @date 2018/08/20
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8899", "http://localhost:9527"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        // 如果所有的属性不全部配置，一定要执行该方法
+        configuration.applyPermitDefaultValues();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
+        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource);
+        return corsFilter;
+    }
+
+}
